aa058a63dfd75be8bf3d36879a015e8bf08aed8c
dzstara <dzstara@pm.me>
2021-03-16 22:24:36 +0100

    More reliable position extraction

--- src/background/hook.ts
+++ src/background/hook.ts
@@ -1,18 +1,9 @@
 import { browser } from "webextension-polyfill-ts";
 
-import { createResponseListener } from "../util/response-listener";
-import { setPosition } from "./radio";
-
-function apiHandler(_: any, responseStr: string) {
-  const response = JSON.parse(responseStr);
-  const rounds = response.rounds;
-  const lastRound = rounds[rounds.length - 1];
-  const { lat, lng } = lastRound;
-  setPosition({ lat, lng });
-}
+import { flushCountry } from "./radio";
 
 browser.webRequest.onBeforeRequest.addListener(
-  createResponseListener(apiHandler),
+  flushCountry,
   {
     urls: ["https://www.geoguessr.com/api/v3/games/*"],
     types: ["xmlhttprequest"],
--- src/content-script/main.ts
+++ src/content-script/main.ts
@@ -1,6 +1,26 @@
 import { browser } from "webextension-polyfill-ts";
 import { Position } from "types";
 
+async function fakeJsonP(uri: string) {
+  const src = uri + "&callback=CB";
+  const response = await fetch(src);
+  const text = await response.text();
+  return JSON.parse(text.slice(14, text.length - 2));
+}
+
+function captureNetworkRequests() {
+  const list = performance
+    .getEntriesByType("resource")
+    .filter((resource: PerformanceResourceTiming) =>
+      ["xmlhttprequest", "script", "img"].includes(resource.initiatorType)
+    )
+    .map((resource) => resource.name);
+
+  performance.clearResourceTimings();
+
+  return list;
+}
+
 function setLocation(location: Position | null) {
   browser.runtime.sendMessage({
     action: "SET_POSITION",
@@ -12,26 +32,38 @@ function resetLocation() {
   setLocation(null);
 }
 
-window.addEventListener("beforeunload", () => {
-  resetLocation();
-});
-
-window.addEventListener("popstate", () => {
+function resetIfNotInGame() {
   if (!window.location.pathname.startsWith("/game")) resetLocation();
-});
-
-setInterval(() => {
-  if (!window.location.pathname.startsWith("/game")) resetLocation();
-}, 100);
-
-try {
-  const raw = document.querySelectorAll("#__NEXT_DATA__")[0].innerHTML;
-  const json = JSON.parse(raw);
-  const rounds = json.props.pageProps.game.rounds;
-  const currentRound = rounds[rounds.length - 1];
-  const { lat, lng } = currentRound;
-
-  setLocation({ lat, lng });
-} catch (err) {
-  console.error(err);
 }
+
+window.addEventListener("beforeunload", resetLocation);
+window.addEventListener("popstate", resetIfNotInGame);
+setInterval(resetIfNotInGame, 100);
+
+async function getPositionFromLastRequests() {
+  const relevantRequests = captureNetworkRequests().filter((url) =>
+    url.includes("GeoPhoto")
+  );
+
+  if (!relevantRequests.length) return;
+
+  const data = await fakeJsonP(relevantRequests[relevantRequests.length - 1]);
+  try {
+    const [lat, lng] = data[1][0][5][0][1][0].slice(2, 4);
+
+    const position = { lat, lng };
+    setLocation(position);
+  } catch (err) {
+    console.error("Could not extract position from payload");
+    console.error("Payload:", data);
+    console.error(err);
+  }
+}
+
+setInterval(async () => {
+  try {
+    await getPositionFromLastRequests();
+  } catch (err) {
+    console.error(err);
+  }
+}, 500);
