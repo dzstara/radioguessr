d03d8d1e78d5807adf64d767b796a2aa3d083cd8
dzstara <dzstara@pm.me>
2021-03-20 18:38:44 +0100

    Add volume icon

--- src/browser-action/components/RadioVolume/index.tsx
+++ src/browser-action/components/RadioVolume/index.tsx
@@ -4,17 +4,25 @@ import Slider from "rc-slider";
 import { useStatus } from "../../contexts/StatusContext";
 
 import "./style.css";
+import { Icon } from "../Icon";
 
 export default function RadioVolume() {
   const { state, setVolume } = useStatus();
 
   return (
-    <Slider
-      min={0}
-      max={1}
-      step={0.001}
-      value={state.volume}
-      onChange={setVolume}
-    />
+    <div className="RadioVolume">
+      <div className="RadioVolume--icon">
+        <Icon name="volume_up" />
+      </div>
+      <div className="RadioVolume--slider">
+        <Slider
+          min={0}
+          max={1}
+          step={0.001}
+          value={state.volume}
+          onChange={setVolume}
+        />
+      </div>
+    </div>
   );
 }
